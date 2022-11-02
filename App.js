62e8689bf56a8cd0595a1abe9360a7d78334b372
“romarpla” <rodrigo.martinez@ember.com.mx>
2022-11-02 00:23:57 -0600

    navigation bar

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React, {useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {NativeBaseProvider, Box} from 'native-base';
+import {NativeBaseProvider, Box, Center} from 'native-base';
 import auth from '@react-native-firebase/auth';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import AppBar from './src/components/AppBar';
 
 import Onboarding from './src/screens/Onboarding/Onboarding';
 
@@ -49,89 +50,94 @@ const App = () => {
     <NativeBaseProvider>
       <NavigationContainer>
         {user ? (
-          <Tabs.Navigator
-            screenOptions={{
-              headerShown: false,
-              tabBarStyle: {
-                position: 'absolute',
-                backgroundColor: 'rgba(249, 249, 249, 1)',
-                marginLeft: '2.5%',
-                borderBottomLeftRadius: 16,
-                borderBottomRightRadius: 16,
-                borderTopLeftRadius: 16,
-                borderTopRightRadius: 16,
-                marginBottom: '5%',
-                height: '9%',
-                width: '95%',
-              },
-              tabBarInactiveTintColor: 'rgba(39, 44, 70, 1)',
-            }}>
-            <Tabs.Screen
-              name="Hábitos"
-              component={Habitos}
-              options={{
-                tarBarLabel: 'Hábitos',
-                tabBarLabelStyle: {
-                  fontSize: 12,
-                  marginBottom: 4,
+          <>
+            <Center>
+              <AppBar />
+            </Center>
+            <Tabs.Navigator
+              screenOptions={{
+                headerShown: false,
+                tabBarStyle: {
+                  position: 'absolute',
+                  backgroundColor: 'rgba(249, 249, 249, 1)',
+                  marginLeft: '2.5%',
+                  borderBottomLeftRadius: 16,
+                  borderBottomRightRadius: 16,
+                  borderTopLeftRadius: 16,
+                  borderTopRightRadius: 16,
+                  marginBottom: '5%',
+                  height: '9%',
+                  width: '95%',
                 },
-                tabBarIcon: ({color, size}) => (
-                  <Box
-                    _pressed={{
-                      backgroundColor: 'rgba(71, 91, 216, 1)',
-                    }}>
-                    <Icon
-                      name="clipboard-check-outline"
-                      color={color}
-                      size={24}
-                    />
-                  </Box>
-                ),
-              }}
-            />
-            <Tabs.Screen
-              name="Enfoque"
-              component={Enfoque}
-              options={{
-                tarBarLabel: 'Enfoque',
-                tabBarLabelStyle: {
-                  fontSize: 12,
-                  marginBottom: 4,
-                },
-                tabBarIcon: ({color, size}) => (
-                  <Icon name="timer-sand-empty" color={color} size={24} />
-                ),
-              }}
-            />
-            <Tabs.Screen
-              name="Finanzas"
-              component={Finanzas}
-              options={{
-                tarBarLabel: 'Finanzas',
-                tabBarLabelStyle: {
-                  fontSize: 12,
-                  marginBottom: 4,
-                },
-                tabBarIcon: ({color, size}) => (
-                  <Icon name="wallet-outline" color={color} size={24} />
-                ),
-              }}
-            />
-            <Tabs.Screen
-              name="Recursos"
-              component={Recursos}
-              options={{
-                tarBarLabel: 'Recursos',
-                tabBarLabelStyle: {
-                  fontSize: 12,
-                  marginBottom: 4,
-                },
-                tabBarIcon: ({color, size}) => (
-                  <Icon name="notebook-outline" color={color} size={24} />
-                ),
-              }}
-            />
-          </Tabs.Navigator>
+                tabBarInactiveTintColor: 'rgba(39, 44, 70, 1)',
+              }}>
+              <Tabs.Screen
+                name="Hábitos"
+                component={Habitos}
+                options={{
+                  tarBarLabel: 'Hábitos',
+                  tabBarLabelStyle: {
+                    fontSize: 12,
+                    marginBottom: 4,
+                  },
+                  tabBarIcon: ({color, size}) => (
+                    <Box
+                      _pressed={{
+                        backgroundColor: 'rgba(71, 91, 216, 1)',
+                      }}>
+                      <Icon
+                        name="clipboard-check-outline"
+                        color={color}
+                        size={24}
+                      />
+                    </Box>
+                  ),
+                }}
+              />
+              <Tabs.Screen
+                name="Enfoque"
+                component={Enfoque}
+                options={{
+                  tarBarLabel: 'Enfoque',
+                  tabBarLabelStyle: {
+                    fontSize: 12,
+                    marginBottom: 4,
+                  },
+                  tabBarIcon: ({color, size}) => (
+                    <Icon name="timer-sand-empty" color={color} size={24} />
+                  ),
+                }}
+              />
+              <Tabs.Screen
+                name="Finanzas"
+                component={Finanzas}
+                options={{
+                  tarBarLabel: 'Finanzas',
+                  tabBarLabelStyle: {
+                    fontSize: 12,
+                    marginBottom: 4,
+                  },
+                  tabBarIcon: ({color, size}) => (
+                    <Icon name="wallet-outline" color={color} size={24} />
+                  ),
+                }}
+              />
+              <Tabs.Screen
+                name="Recursos"
+                component={Recursos}
+                options={{
+                  tarBarLabel: 'Recursos',
+                  tabBarLabelStyle: {
+                    fontSize: 12,
+                    marginBottom: 4,
+                  },
+                  tabBarIcon: ({color, size}) => (
+                    <Icon name="notebook-outline" color={color} size={24} />
+                  ),
+                }}
+              />
+            </Tabs.Navigator>
+          </>
         ) : (
           <AuthStack.Navigator options={{headerShown: false}}>
             <AuthStack.Screen
