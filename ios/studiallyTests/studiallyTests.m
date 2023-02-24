214727b5a8415198e5a9482c36b2419124a39189
David Silis <david.silis@ember.com.mx>
2023-02-23 22:33:20 -0600

    style

--- ios/studiallyTests/studiallyTests.m
+++ ios/studiallyTests/studiallyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface studiallyTests : XCTestCase
+@interface StudiallyTests : XCTestCase
 
 @end
 
-@implementation studiallyTests
+@implementation StudiallyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
